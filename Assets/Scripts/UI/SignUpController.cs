656dc8e40fabded88f73245b58d2fe63f18a9c07
Denis Zhukovin <pavekzh@gmail.com>
2023-06-28 16:03:04 +0300

    Installed PlayFab, installed TMPro

---
+++ Assets/Scripts/UI/SignUpController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class SignUpController:MonoBehaviour
+{
+    [SerializeField] private TMP_InputField email;
+    [SerializeField] private TMP_InputField username;
+    [SerializeField] private TMP_InputField password;
+    [SerializeField] private TMP_InputField repeatPassword;
+    [SerializeField] private Button signUp;
+    [SerializeField] private Button logIn;
+
+}
